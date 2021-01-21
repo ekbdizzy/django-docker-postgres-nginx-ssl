eb24746ccaaa50f7c0f422ea3ce98a3f4c2cb36a
Aleksey Koshkin <ekbdizzy@yandex.ru>
2021-01-21 16:29:17 +0300

    changed settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -1,15 +1,16 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv
+from environs import Env
 
-load_dotenv(find_dotenv('.env'))
+env = Env()
+env.read_env(find_dotenv('.env'))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_SECRET_KEY = 'dq+sh59b@7+91zd(vx0iwr^h59_$ezd(m9r5+k)0vdgna-a)ka'
-SECRET_KEY = os.environ.get('SECRET_KEY', TEMPLATE_SECRET_KEY)
+SECRET_KEY = env('SECRET_KEY', TEMPLATE_SECRET_KEY)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+DEBUG = env.bool('DEBUG', False)
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
